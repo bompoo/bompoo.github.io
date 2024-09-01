dea24efdc53c596cb469455c8e6c80b7da19ba42
bompoo <bompoo@outlook.com>
2024-09-01 16:07:07 +0800

    feat: custom commit

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['chore', 'docs', 'feat', 'fix', 'refactor', 'addLog', 'test'],
-    ],
-  },
+  // extends: ['@commitlint/config-conventional'],
+  // rules: {
+  //   'type-enum': [
+  //     2,
+  //     'always',
+  //     ['chore', 'docs', 'feat', 'fix', 'refactor', 'addLog', 'test'],
+  //   ],
+  // },
+  extends: ['cz'],
+  rules: {},
 };
